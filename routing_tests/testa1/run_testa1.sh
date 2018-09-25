37ec040a5cf1f730e1eb255fee015e0bfe1e6e21
Leo Liu <leoszliu@gmail.com>
2018-09-25 17:48:01 -0400

    Add further fixes on testa1 run file

--- routing_tests/testa1/run_testa1.sh
+++ routing_tests/testa1/run_testa1.sh
@@ -13,11 +13,12 @@ cp src/main/reg_bank/reg_1d_4b_1.dcp src/main/resources/components/type1.dcp
 cp src/main/reg_bank/reg_1d_2b_0.dcp src/main/resources/components/type2.dcp
 cp src/main/reg_bank/reg_1d_2b_1.dcp src/main/resources/components/type3.dcp
 
+cp routing_tests/testa1/components.conf src/main/resources/register_components.conf
+
 for bit in "${bw[@]}"
 do
     for sep in "${dist[@]}"
     do
-        cp routing_tests/testa1/components/testa1_"$bit"b_comp_0.conf src/main/resources/register_components.conf
         cp routing_tests/testa1/placements/testa1_"$bit"b_place_N"$sep"_0.conf src/main/resources/placements.conf
         cp routing_tests/testa1/routes/testa1_"$bit"b_route.conf src/main/resources/routes.conf
 
@@ -25,7 +26,6 @@ do
         time java CustomDesign --out regpair_"$bit"b_N"$sep"_0
         date >> routing_tests/testa1/results.txt
 
-        cp routing_tests/testa1/components/testa1_"$bit"b_comp_1.conf src/main/resources/register_components.conf
         cp routing_tests/testa1/placements/testa1_"$bit"b_place_N"$sep"_1.conf src/main/resources/placements.conf
         cp routing_tests/testa1/routes/testa1_"$bit"b_route.conf src/main/resources/routes.conf
 
@@ -33,7 +33,6 @@ do
         time java CustomDesign --out regpair_"$bit"b_N"$sep"_1
         date >> routing_tests/testa1/results.txt
 
-        cp routing_tests/testa1/components/testa1_"$bit"b_comp_0.conf src/main/resources/register_components.conf
         cp routing_tests/testa1/placements/testa1_"$bit"b_place_S"$sep"_0.conf src/main/resources/placements.conf
         cp routing_tests/testa1/routes/testa1_"$bit"b_route.conf src/main/resources/routes.conf
 
@@ -42,7 +41,6 @@ do
         date >> routing_tests/testa1/results.txt
 
 
-        cp routing_tests/testa1/components/testa1_"$bit"b_comp_1.conf src/main/resources/register_components.conf
         cp routing_tests/testa1/placements/testa1_"$bit"b_place_S"$sep"_1.conf src/main/resources/placements.conf
         cp routing_tests/testa1/routes/testa1_"$bit"b_route.conf src/main/resources/routes.conf
 

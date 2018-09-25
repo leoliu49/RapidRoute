8ca1d3fb0a90b6e790a4d92389f6216f6176dd8d
Leo Liu <leoszliu@gmail.com>
2018-09-25 17:42:35 -0400

    Fix testa1 to be adding reg_bank DCPs (previous tests broken)

--- routing_tests/testa1/run_testa1.sh
+++ routing_tests/testa1/run_testa1.sh
@@ -8,6 +8,11 @@ declare -a dist=( 1 4 16 64 )
 rm -f routing_tests/testa1/results.txt
 cd ../../
 
+cp src/main/reg_bank/reg_1d_4b_0.dcp src/main/resources/components/type0.dcp
+cp src/main/reg_bank/reg_1d_4b_1.dcp src/main/resources/components/type1.dcp
+cp src/main/reg_bank/reg_1d_2b_0.dcp src/main/resources/components/type2.dcp
+cp src/main/reg_bank/reg_1d_2b_1.dcp src/main/resources/components/type3.dcp
+
 for bit in "${bw[@]}"
 do
     for sep in "${dist[@]}"
@@ -19,7 +24,6 @@ do
         date >> routing_tests/testa1/results.txt
         time java CustomDesign --out regpair_"$bit"b_N"$sep"_0
         date >> routing_tests/testa1/results.txt
-
 
         cp routing_tests/testa1/components/testa1_"$bit"b_comp_1.conf src/main/resources/register_components.conf
         cp routing_tests/testa1/placements/testa1_"$bit"b_place_N"$sep"_1.conf src/main/resources/placements.conf
